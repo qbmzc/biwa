3c71fa2eda266e9116737ab702a530ec3a9d2a94
cong <congco@foxmail.com>
2021-09-23 17:01:34 +0800

    feat:12121

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
-from flask import Flask, request, jsonify,render_template
+from flask import Flask, request, jsonify, render_template
 import json
+import logging
+import folium
+from folium import plugins
+import time, os, uuid
 
 app = Flask(__name__)
+
+
+@app.route('/displaygeom')
+def displaygeom():
+    arr = [(113.54, 34.7), (113.54, 34.75), (113.55, 34.85)]
+    html_file = displaygeom(lines=arr, points=arr)
+    return render_template(html_file)
+
+
+def displaygeom(lines, points):
+    lines = [_[::-1] for _ in lines]
+    num = len(lines)
+    center_x = sum([_[0] for _ in lines]) / num
+    center_y = sum([_[1] for _ in lines]) / num
+
+    m = folium.Map(location=[center_x, center_y],
+                   zoom_start=14,
+                   width='100%',
+                   height='100%',
+                   zoom_control='False',
+                   tiles='http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}&ltype=6',
+                   attr='AutoNavi')
+    # folium.PolyLine(locations=points, color='green').add_to(m)
+    folium.plugins.AntPath(lines, dash_array=[20, 25]).add_to(m)
+
+    points = [_[::-1] for _ in points]
+    for p in points:
+        folium.Marker(p).add_to(m)
+    filename = "{}_{}.html".format("test", str(uuid.uuid1())[:8])
+    logging.info("filename" + str(filename))
+    outfile = "./templates/" + str(filename)
+    logging.warning(f'out_file:{outfile}')
+    m.save(outfile)
+    return filename
 
 
 @app.route("/map", methods=['post'])
@@ -14,7 +52,7 @@ def getLngLat():
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 
